28415d3951a1b624ea7a2c4ff6bade2d92f25c4f
javex <florian.ruechel@inexplicity.de>
2014-10-14 14:34:08 +0200

    Bump version number

--- fluxscoreboard/__init__.py
+++ fluxscoreboard/__init__.py
@@ -15,7 +15,7 @@ from sqlalchemy import engine_from_config
 import warnings
 
 
-__version__ = '0.4.5'
+__version__ = '0.4.6'
 # ALWAYS make an exception for a warning (from sqlalchemy)
 warnings.filterwarnings("error", category=Warning, module=r'.*sqlalchemy.*')
 
